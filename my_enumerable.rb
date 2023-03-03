1b0c51c152ec1460cf3e3f84012dba01403dc7b0
elarhadu <elarhadu@gmail.com>
2023-03-03 14:57:52 +0000

    Add MyEnumerable module

---
+++ my_enumerable.rb
@@ -0,0 +1,21 @@
+module MyEnumerable
+  #all
+  def all?
+    each { |item| return false unless yield item }
+    true
+  end
+
+  #any
+  def any?
+    each  {|item| return true if yield(item)}
+    false
+  end
+
+  #filter
+  def filter?(&block)
+    result = []
+    each { |e| result << e  if yield e }
+    result
+  end
+
+end
